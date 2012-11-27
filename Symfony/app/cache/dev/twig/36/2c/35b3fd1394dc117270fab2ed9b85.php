9173dd143e373e10af1ab0e99b87c7b8ce7b1eb0
adox <adam2c@poczta.onet.pl>
2012-11-27 19:45:09 +0100

    First commit

---
+++ Symfony/app/cache/dev/twig/36/2c/35b3fd1394dc117270fab2ed9b85.php
@@ -0,0 +1,39 @@
+<?php
+
+/* AcmeHelloBundle:Default:index.html.twig */
+class __TwigTemplate_362c35b3fd1394dc117270fab2ed9b85 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello ";
+        echo twig_escape_filter($this->env, $this->getContext($context, "name"), "html", null, true);
+        echo "!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "AcmeHelloBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  17 => 1,);
+    }
+}
